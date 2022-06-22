c34f661ddb8c85497f051f6604124e89cf77273c
Gautam Tiwari <gautamtiwari142@gmail.com>
2022-06-22 23:21:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_API_KEY = "90GK147J4SFXP4SD"
-NEW_API_KEY = "b256b9fc3bb044f5b38f785df84051e3"
-TWILIO_SID = "AC4327cfa6f6cb0ce4707e396e63f1aa49"
-TWILIO_AUTH_TOKEN = "72a5c95d729381a399636ec78cdb38ab"
+STOCK_API_KEY = ""
+NEW_API_KEY = ""
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
+FROM_NUMBER = ""
+TO_NUMBER = ""
 
 stock_params = {
     'function': 'TIME_SERIES_DAILY',
@@ -55,6 +57,6 @@ if diff_percent > 5:
     for article in formatted_articles:
         message = client.messages.create(
             body=article,
-            from_="+13514449832",
-            to="+917317464093"
+            from_=FROM_NUMBER,
+            to=TO_NUMBER
         )
